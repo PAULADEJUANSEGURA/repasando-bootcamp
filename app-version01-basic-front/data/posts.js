e59636e266a69522c8f3bf0dcbf725282a089e8c
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2024-01-02 10:41:45 +0100

    cambio en array de posts feliz navidad dice Ada Lovelace

--- app-version01-basic-front/data/posts.js
+++ app-version01-basic-front/data/posts.js
@@ -9,6 +9,6 @@ posts.push(
 
 posts.push(
     {id: ++posts.count, 
-    image: 'https://i.pinimg.com/originals/dc/95/b8/dc95b87682d41472c718760bd3a1d21e.jpg', 
-    text: 'Jimi Hendrix', 
+    image: 'https://img.freepik.com/vector-gratis/fondo-navidad-diseno-plano-personajes_52683-48584.jpg', 
+    text: 'Viva la Navidad!! Felices fiestas!!', 
     user: users[1].id})
