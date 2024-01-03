5b1c3d80a485faad28be197d323e8f24c6943cab
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2024-01-03 18:25:20 +0100

    migrando la app a componentes de React

--- app-version02-introducing-compos-React/main.jsx
+++ app-version02-introducing-compos-React/main.jsx
@@ -1,15 +1,7 @@
-/* Con la instalacion de BABEL StandAlone */
+// globals
 
-const root = document.querySelector('#root');
+let userId = null;
 
-const virtualRoot = ReactDOM.createRoot(root);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const title = <h1>Hola mundo desde REACT con BABEL</h1>
-
-const color1 = <li>Color ROJO</li>
-const color2 = <li>Color AZUL</li>
-const color3 = <li>Color NARANJA</li>
-
-const colors = <ul> {color1} {color2} {color3} </ul>
-
-virtualRoot.render([title, colors]);
+root.render(<App />);
---
+++ app-version02-introducing-compos-React/view/pages/Login.jsx
@@ -0,0 +1,20 @@
+function Login (){
+    return <main className="login-view off">
+      <h1>Login</h1>
+      <form action="submit" className="login-form">
+        <label htmlFor="login-email" type="text">Email</label>
+        <input type="text" name="email" id="login-email" placeholder="e-mail"></input>
+
+        <label htmlFor="login-password" type="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="login-password"
+          placeholder="password"
+        ></input>
+
+        <button type="submit">Login</button>
+      </form>
+      <p>Go to <a class="login-register-link" href="">Register</a></p>
+    </main>
+    }
